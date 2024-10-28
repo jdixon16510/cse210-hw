402dca8f246919397957d79790528de2e2fb4e1c
Joseph Dixon <jdixon@horncadcenter.com>
2024-10-28 16:26:55 -0600

    Create program including the stretch functionality.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        int guess = 0;
+        int count = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+            count++;
+            if (guess > magic)
+            {
+                Console.WriteLine($"Your guess {guess} is higher then the magic number.");
+            }
+            else if (guess < magic)
+            {
+                Console.WriteLine($"Your guess {guess} is lower then the magic number.");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed the magic number {magic} in {count} tries.");
+            }
+        } while (guess != magic);
     }
 }
