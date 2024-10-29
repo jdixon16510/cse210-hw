2d92c91cf541deba0603ee69f6054a3ab41bcd59
Joseph Dixon <jdixon@horncadcenter.com>
2024-10-29 17:33:20 -0600

    Resume Program Creation

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Sofware Engineer";
+        job1._startYear = "1975";
+        job1._endYear = "2024";
+        
+        Job job2 = new Job();
+        job2._company = "Oracle";
+        job2._jobTitle = "Civil Engineer";
+        job2._startYear = "1995";
+        job2._endYear = "2016";
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Joseph Dixon";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
