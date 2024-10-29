00531c48b1e515a400fd8323aa244f92efe6db25
Joseph Dixon <jdixon@horncadcenter.com>
2024-10-29 12:28:03 -0600

    Craeated the program with the stretch challenge requirements.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int number = 1;
+        List<int> numbers;
+        numbers = new List<int>();
+        int sum = 0;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        do 
+        {
+            Console.Write("Enter a number. ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        foreach (int x in numbers)
+        {
+            sum = sum + x;
+            
+        }
+
+        Console.WriteLine($"The sum is {sum}");
+
+        int count = numbers.Count;
+        float average = (float)sum / (float)count;
+        Console.WriteLine($"The average is {average}");
+
+        int largest = 0;
+
+        foreach (int x in numbers)
+        {
+            if (x > largest)
+            {
+                largest = x;
+            }
+        }
+        Console.WriteLine($"The largest number is {largest}");
+
+        int smallest = largest + 1;
+
+        foreach (int x in numbers)
+        {
+            if (x > 0 && x < smallest)
+            {
+                smallest = x;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is {smallest}");
+
+        numbers.Sort();
+        foreach (int x in numbers)
+        {
+           Console.WriteLine(x); 
+        }
+
     }
 }
