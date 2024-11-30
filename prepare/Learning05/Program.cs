aea378cf1c69fd1de5cda55ba8b036b123523987
Joseph Dixon <jdixon@horncadcenter.com>
2024-11-30 15:05:18 -0700

    New program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Joseph Dixon", "C# Programming");
+        Console.WriteLine (assignment1.GetSummary());
+
+        MathAssignment mathAssignment1 = new MathAssignment("Joseph Dixon", "Math", "74", "2-17");
+        Console.WriteLine (mathAssignment1.GetSummary());
+        Console.WriteLine (mathAssignment1.GethomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Joseph Dixon", "Writing", "Big Blue Mable");
+        Console.WriteLine (writingAssignment1.GetSummary());
+        Console.WriteLine (writingAssignment1.GetWritingInformation());
     }
 }
